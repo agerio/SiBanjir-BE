78f5645792a8dbe555e27ea37622c1bb15d50c9f
ghaziangunawan <ghazian2603@gmail.com>
2024-08-22 15:02:52 +1000

    change project name to lowercase

--- SiBanjir/wsgi.py
+++ SiBanjir/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for SiBanjir project.
+WSGI config for sibanjir project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'SiBanjir.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sibanjir.settings')
 
-app = get_wsgi_application()
+application = get_wsgi_application()
