3473ee76d5500e362aeb6b3b85f444d17cd44021
Jovian <ajovian440@gmail.com>
2024-08-22 14:46:58 +1000

    Add vercel's domain to ALLOWED_HOSTS

--- SiBanjir/settings.py
+++ SiBanjir/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure--^#1af!)31m8)gyzny52by@*a*@4%r27qeao#(-jw&04@+%1h1
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app']
 
 
 # Application definition
