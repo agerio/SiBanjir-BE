96f443c838cbefdde81fe7a74e808e1d0f3ee900
ghaziangunawan <ghazian2603@gmail.com>
2024-08-22 17:29:32 +1000

    a

--- build_files.sh
+++ build_files.sh
@@ -9,8 +9,6 @@ source venv/bin/activate
 # install all deps in the venv
 pip install -r requirements.txt
 
-# collect static files using the Python interpreter from venv
-python manage.py collectstatic --noinput
 
 
 echo "BUILD END"
