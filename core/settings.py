e99b5c5bf9994f75b630301925f5a9d2b4f8d301
Jovian <ajovian440@gmail.com>
2024-08-22 15:34:03 +1000

    Add example

--- core/settings.py
+++ core/settings.py
@@ -25,12 +25,13 @@ SECRET_KEY = 'django-insecure-w)*io-iw$^--vep!vdzf8z=zn9wirvk-+hiss+)70%p9d6b8xs
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app']
+ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app']
 
 
 # Application definition
 
 INSTALLED_APPS = [
+    'example',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
