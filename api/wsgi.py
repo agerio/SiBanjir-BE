9f746cfd0cf673f5ca19d7c2d3fae550916660a9
Jovian <ajovian440@gmail.com>
2024-08-22 16:05:46 +1000

    Rename django project to api

--- api/wsgi.py
+++ api/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for core project.
+WSGI config for api project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -9,9 +9,9 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 
 import os
 
-from django.core.wsgi import get_wsgi_application
+from django.api.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'api.settings')
 
 application = get_wsgi_application()
 app = application
