9671fc01cad0525459c096bb8920519ca1e14a6f
Jovian <ajovian440@gmail.com>
2024-08-23 10:03:42 +1000

    Added makemigrations to build

--- build_files.sh
+++ build_files.sh
@@ -9,6 +9,7 @@ source venv/bin/activate
 # install all deps in the venv
 pip install -r requirements.txt
 python3.9 manage.py collectstatic
+python3.9 manage.py makemigrations
 python3.9 manage.py migrate
 python3.9 manage.py loaddata initial_helloworld_data.json
 
