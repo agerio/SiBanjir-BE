03314e90c9f11b5663b8a50ee5e713b5985e76f2
ghaziangunawan <ghazian2603@gmail.com>
2024-08-28 23:28:59 +1000

    add user registration

---
+++ user/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+
+# Create your views here.
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .serializer import UserRegistrationSerializer
+
+class UserRegistrationView(APIView):
+    def post(self, request, *args, **kwargs):
+        serializer = UserRegistrationSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({"message": "User created successfully"}, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    def get(self ,request):
+        return Response("hello world")
+
