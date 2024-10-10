3657bbc91921b5f52136e2ea1385c1754c8615a1
christiantoDavid <david.christianto05@gmail.com>
2024-10-10 21:41:44 +1000

    push ke main, ubah model special warning attribute

--- specialwarning/models.py
+++ specialwarning/models.py
@@ -4,8 +4,8 @@ from cloudinary.models import CloudinaryField
 
 class SpecialFloodWarning(models.Model):
     name = models.CharField(max_length=255)
-    long = models.DecimalField(max_digits=19, decimal_places=5)
-    lat = models.DecimalField(max_digits=19, decimal_places=5)
+    long = models.DecimalField(max_digits=19, decimal_places=10)
+    lat = models.DecimalField(max_digits=19, decimal_places=10)
     created_by = models.ForeignKey(User, related_name='created_by', on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     verified_by = models.ManyToManyField(User, null=True, blank=True)
