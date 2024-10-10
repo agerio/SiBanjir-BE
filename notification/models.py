0a763f807c047fb84a5aecb3200cd9d97f44e9f2
Heraldoe <halomoangeraldo2@gmail.com>
2024-10-10 19:41:23 +1000

    fix related name issue

--- notification/models.py
+++ notification/models.py
@@ -4,4 +4,4 @@ from django.contrib.auth.models import User
 class NotificationHistory(models.Model):
     name = models.CharField(max_length=255)
     created_at = models.DateTimeField(auto_now_add=True)
-    created_by = models.ForeignKey(User, related_name='created_by', on_delete=models.CASCADE)
+    created_by = models.ForeignKey(User, related_name='notif_created_by', on_delete=models.CASCADE)
