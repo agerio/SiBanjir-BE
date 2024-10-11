8f30a9650c19226afae00324ac9872ac836bec2b
christiantoDavid <david.christianto05@gmail.com>
2024-10-11 21:24:44 +1000

    ubah max digit

--- specialwarning/models.py
+++ specialwarning/models.py
@@ -4,8 +4,8 @@ from cloudinary.models import CloudinaryField
 
 class SpecialFloodWarning(models.Model):
     name = models.CharField(max_length=255)
-    long = models.DecimalField(max_digits=19, decimal_places=18)
-    lat = models.DecimalField(max_digits=19, decimal_places=18)
+    long = models.DecimalField(max_digits=20, decimal_places=18)
+    lat = models.DecimalField(max_digits=20, decimal_places=18)
     created_by = models.ForeignKey(User, related_name='created_by', on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     verified_by = models.ManyToManyField(User, null=True, blank=True)
