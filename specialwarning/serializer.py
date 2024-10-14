cbb225c32f6da9bae9433d6c8ed8ceb167576cf6
Jovian <ajovian440@gmail.com>
2024-10-14 16:32:42 +1000

    Update special warning serializer

--- specialwarning/serializer.py
+++ specialwarning/serializer.py
@@ -2,20 +2,18 @@ from rest_framework import serializers
 from .models import SpecialFloodWarning
 
 class SpecialFloodWarningSerializer(serializers.ModelSerializer):
+    created_by = serializers.SerializerMethodField()
+    profile_picture = serializers.SerializerMethodField()
 
     class Meta:
         model = SpecialFloodWarning
         fields = '__all__'
 
-    def get_latlong(self, obj):
-        return f"{obj.latitude}, {obj.longitude}"
+    def get_created_by(self, obj):
+        return obj.created_by.username
 
-    def to_representation(self, instance):
-        representation = super().to_representation(instance)
-
-        if instance.image:
-            representation['image'] = instance.image.url
-        else:
-            representation['image'] = None
-
-        return representation
+    def get_profile_picture(self, obj):
+        profile = getattr(obj.created_by, 'profile', None)
+        if profile and profile.profile_picture:
+            return profile.profile_picture.url
+        return None
